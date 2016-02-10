a5c6e02adcf0da1dd6e80b2cc0cf39849ffbb065
gluc <christoph.glur@ipub.com>
2016-02-10 13:44:47 +0000

    edges defaults ... but some tests are still failing :-(

--- R/node_plot.R
+++ R/node_plot.R
@@ -33,6 +33,7 @@ plot.Node <- function(x, direction = c("climb", "descend"), pruneFun = NULL, eng
 #' #inheritance can be avoided:
 #' SetNodeStyle(acme$Accounting, inherit = FALSE, fillcolor = "Thistle", 
 #'              fontcolor = "Firebrick", tooltip = "This is the accounting department")
+#' SetEdgeStyle(acme$Research$`New Labs`, color = "red", label = "Focus!", penwidth = 3, fontcolor = "red")
 #' #use Do to set style on specific nodes:
 #' Do(acme$leaves, function(node) SetNodeStyle(node, shape = "egg"))
 #' plot(acme)
@@ -50,7 +51,7 @@ ToGraphViz <- function(root, direction = c("climb", "descend"), pruneFun = NULL)
   if (!anu && !"label" %in% ns ) ns <- c(ns, "label")
   for (style in ns) {
     myargs[[style]] <- Get(tr, function(x) {
-      myns <- GetNodeStyle(x, style)
+      myns <- GetStyle(x, style, "node")
       if (style == "label" && !anu && length(myns) == 0) myns <- x$name
       if (is.null(myns)) myns <- ""
       myns
@@ -63,21 +64,6 @@ ToGraphViz <- function(root, direction = c("climb", "descend"), pruneFun = NULL)
   #nodes <- nodes[!names(nodes)=="tooltip"]
   
   ns <- unique(unlist(sapply(root$Get(function(x) attr(x, "edgeStyle"), simplify = FALSE), names)))
-  
-  
-  GetEdgeStyleFactory <- function(style) {
-    styleName <- force(style)
-    function(node = node, origNode = node) {
-      inh <- attr(node, "edgeStyleInherit")
-      res <- attr(node, "edgeStyle")[[style]]
-      if (!is.null(res) && (identical(node, origNode) || inh)) {
-        if (is.function(res)) res <- res(origNode)
-        return (res)
-      }
-      if (node$isRoot || styleName %in% c("label", "tooltip")) return ("")
-      return (Recall(node = node$parent, origNode = origNode))
-    }
-  }
   
   
   myargs <- list()
@@ -93,8 +79,9 @@ ToGraphViz <- function(root, direction = c("climb", "descend"), pruneFun = NULL)
   graphStyle <- attr(root, "graphStyle")
   if (!is.null(graphStyle)) graphAttributes <- paste(names(graphStyle), paste0("'", graphStyle, "'"), sep = " = ", collapse = ", ")
   else graphAttributes <- ""
-  nodeAttributes <- GetNodeDefaultStyles(root)
-  graph <- create_graph(nodes, edges, graph_attrs = graphAttributes, node_attrs = nodeAttributes)
+  nodeAttributes <- GetDefaultStyles(root, type = "node")
+  edgeAttributes <- GetDefaultStyles(root, type = "edge")
+  graph <- create_graph(nodes, edges, graph_attrs = graphAttributes, node_attrs = nodeAttributes, edge_attrs = edgeAttributes)
   
   #return (graph)
   #render_graph(graph)
@@ -102,3 +89,100 @@ ToGraphViz <- function(root, direction = c("climb", "descend"), pruneFun = NULL)
   return (graph$dot_code)
   
 }
+
+
+
+GetEdgeStyleFactory <- function(style) {
+  style <- force(style)
+  function(node = node, origNode = node) {
+    myns <- GetStyle(node, style, "edge")
+    if (is.null(myns)) myns <- ""
+    myns
+  }
+}
+
+
+#' @param inherit If TRUE, then children will inherit this node's style. Otherwise they inherit from this 
+#' node's parent.
+#' 
+#' @rdname ToGraphViz
+#' 
+#' @export
+SetNodeStyle <- function(node, 
+                         inherit = TRUE,
+                         ...) {
+  ll <- list(...)
+  attr(node, "nodeStyle") <- ll
+  attr(node, "nodeStyleInherit") <- inherit
+}
+
+
+#' @rdname ToGraphViz
+#' @export
+SetEdgeStyle <- function(node,
+                         inherit = TRUE,
+                         ...) {
+  ll <- list(...)
+  attr(node, "edgeStyle") <- ll
+  attr(node, "edgeStyleInherit") <- inherit
+}
+
+
+#' @rdname ToGraphViz 
+#' @export
+SetGraphStyle <- function(root,
+                          ...) {
+  ll <- list(...)
+  attr(root, "graphStyle") <- ll
+}
+
+
+GetStyle <- function(node, styleName, type = c("node", "edge"), origNode = node) {
+  type <- type[1]
+  inh <- attr(node, paste0(type, "StyleInherit"))
+  res <- attr(node, paste0(type, "Style"))[[styleName]]
+  if (!is.null(res)) {
+    if (!node$isRoot) {
+      if (identical(node, origNode) || inh) {# either on myself or inheritable
+        if (is.function(res)) res <- res(origNode)
+        return (res)
+      }
+    } else {
+      #root
+      if (identical(node, origNode) && styleName %in% c("label", "tooltip")) {
+        if (is.function(res)) res <- res(origNode)
+        return (res)
+      } else {
+        #inherited are only label and tt, and only if function
+        if (styleName %in% c("label", "tooltip") && is.function(res)) {
+          return (res(origNode))
+        }
+      }
+      
+    }
+  }
+  #recursion exit criteria
+  # inherit from root only if label or tt are function
+  if (node$level == 2 && styleName %in% c("label", "tooltip")) {
+    res <- GetStyle(node$parent, styleName, type, origNode = origNode)
+    if (is.function(res)) res <- res(origNode)
+    return (res)
+  }
+  if (node$level <= 2) return (NULL) 
+  #recursion
+  GetStyle(node$parent, styleName, type, origNode = origNode)
+  
+}
+
+GetDefaultStyles <- function(node, type = c("node", "edge")) {
+  type <- type[1]
+  node <- node$root
+  inh <- attr(node, paste0(type, "StyleInherit"))
+  res <- attr(node, paste0(type, "Style"))
+  if (!is.null(res) && inh) {
+    res <- res[!names(res) %in% c("label", "tooltip")]
+    res <- paste(names(res), paste0("'", res, "'"), sep = " = ", collapse = ", ")
+    return (res) 
+  } else return (NULL)
+}
+
